2f9ee4e7e13486f39453e8a9a3a155c842e20ef7
Amid <48596058+Amidn@users.noreply.github.com>
2026-04-27 15:45:17 +0200

    Update ingestor.py

--- ingestor.py
+++ ingestor.py
@@ -2,7 +2,8 @@
 
 import os
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 
